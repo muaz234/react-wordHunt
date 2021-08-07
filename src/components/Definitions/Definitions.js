fe962cdf3a5a29fe80e90729d2ad1acd28fcd3d8
muaz234 <ahmedmuaz0152@gmail.com>
2021-08-07 12:42:10 +0800

    update Definitions

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -4,7 +4,7 @@ const Definitions = ({ word, category, meanings, LightMode }) => {
     return (
         <div className="meanings">
             {meanings[0] && word && category === "en" && (
-                <audio src={meanings[0].phonetics[0]} style={{backgroundColor: '#fff', borderRadius: 10 }}>
+                <audio src={meanings[0].phonetics[0] &&  meanings[0].phonetics[0].audio} style={{backgroundColor: '#fff', borderRadius: 10 }}>
                     Your browser does not support audio element
                     </audio>
             )}
